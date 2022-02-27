24338337d12e82a618348b4b66e5c82a759abc96
JAMAL EL KHDADI <57815023+jamalelkhdadi@users.noreply.github.com>
2022-02-27 16:22:39 +0100

    Update script.js

--- dist/script.js
+++ dist/script.js
@@ -1,1 +1,16 @@
 
+// 01 Scroll to top
+let span = document.querySelector(".up");
+window.onscroll = function () {
+    if (this.scrollY >= 800) {
+      span.classList.add("show");
+    } else {
+      span.classList.remove("show");
+    }
+};
+span.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+};
